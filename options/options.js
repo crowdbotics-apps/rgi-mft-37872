0ad05b30eb1d516852e13eb5af5879679fb29c4e
Cuan Mumble <cuan.mumble@crowdbotics.com>
2022-12-19 18:52:38 +0200

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native";
-
 export const globalOptions = {
   name: "rgi_mft_37872",
   url: "https://rgi_mft_37872.botics.co",
   api: "https://rgi_mft_37872.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     "copy": "Routes available!"
   }
-}
+};
